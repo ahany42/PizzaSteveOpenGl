d001e191202281d9b132306aaf743959633db340
ahany42 <ahanyfathy42@gmail.com>
2025-03-09 03:06:58 +0200

    mile stone 2 part one

--- Graphics/2022170826.cs
+++ Graphics/2022170826.cs
@@ -13,152 +13,247 @@ using System.Text;
 using System.Threading.Tasks;
 using Tao.OpenGl;
 using GlmNet;
-//include GLM library
-
-
 using System.IO;
-
+using System.Diagnostics;
 namespace Graphics
+
 {
     class Renderer
     {
         Shader sh;
-        uint vertexBufferID;
 
-        //3D Drawing
-        mat4 m;
-        mat4 v;
-        mat4 p;
-        mat4 mvp;
-        int MVP_ID;
+        uint cheeseBufferID;
+        uint armsBufferID;
+        uint legsBufferID;
+        uint mouthBufferID;
+        uint eyesBufferID;
+   
+        mat4 ModelMatrix;
+        mat4 ViewMatrix;
+        mat4 ProjectionMatrix;
+
+        int ShaderModelMatrixID;
+        int ShaderViewMatrixID;
+        int ShaderProjectionMatrixID;
+
+        const float rotationSpeed = 1f;
+        float rotationAngle = 0;
+
+        public float translationX = 0,
+                     translationY = 0,
+                     translationZ = 0;
+
+        Stopwatch timer = Stopwatch.StartNew();
+
+        vec3 triangleCenter;
+
         public void Initialize()
         {
             string projectPath = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
             sh = new Shader(projectPath + "\\Shaders\\SimpleVertexShader.vertexshader", projectPath + "\\Shaders\\SimpleFragmentShader.fragmentshader");
             Gl.glClearColor(0, 0, 0.4f, 1);
-            float[] verts = {
-         // Cheese
-          -1.0f,  -1.0f,  0.2f,
-          1.0f, 0.8f, 0.3f,//RGB
-          1.0f,  -1.0f,  0.2f,  
-          1.0f, 0.8f, 0.3f,//RGB
-          0.0f, 1.0f,  0.2f,  
-          1.0f, 0.8f, 0.3f,//RGB
 
-         // Pizza 
-         -1.0f,  -1.0f, -0.2f, 
-         1.0f, 0.6f, 0.3f,//RGB
-         1.0f,  -1.0f, -0.2f,
-         1.0f, 0.6f, 0.3f,//RGB
-         0.0f, 1.0f, -0.2f,
-        1.0f, 0.6f, 0.3f,//RGB  
+            triangleCenter = new vec3(-1.0f, -1.0f, -0.2f);
+            float[] cheeseVerts = {
+            -1.0f,  -1.0f,  0.2f,
+            1.0f, 0.6f, 0.3f,//RGB
+            1.0f,  -1.0f,  0.2f,
+            1.0f, 0.6f, 0.3f,//RGB
+            0.0f, 1.0f,  0.2f,
+            1.0f, 0.6f, 0.9f//RGB 
+            };
+            float[] eyesVerts = {
+            -0.5f,-0.7f,0.2f,
+            0.0f,0.0f,0.0f,//RGB
+            0.5f,-0.7f,0.2f,
+            0.0f,0.0f,0.0f,//RGB
+            };
+            float[] mouthVerts = {        
+             // Mouth Quad - 2 Triangles
+             -0.2f, 0.2f, 0.2f,
+             0.0f, 0.0f, 0.0f,//RGB  
+             0.2f, 0.2f, 0.2f,
+             0.0f, 0.0f, 0.0f,//RGB  
+             0.2f, 0.4f, 0.2f,
+             0.0f, 0.0f, 0.0f,//RGB  
+             -0.2f, 0.2f, 0.2f,
+             0.0f, 0.0f, 0.0f,//RGB  
+             0.2f, 0.4f, 0.2f,
+             0.0f, 0.0f, 0.0f,//RGB 
+             -0.2f, 0.4f, 0.2f,
+             0.0f, 0.0f, 0.0f,//RGB
+            };
+          
+            float[] armsVerts = {
+                
+             //Right Arm
+             -0.5f,0.0f,0.2f,
+             0.0f,0.0f,0.0f,//RGB
+             -1.4f,-1.0f,0.2f,
+             0.0f,0.0f,0.0f,//RGB
+                
+             //Left Arm
+             0.5f,0.0f,0.2f,
+             0.0f,0.0f,0.0f,//RGB
+             1.4f,-1.0f,0.2f,
+             0.0f,0.0f,0.0f,//RGB
+            };
+            float[] legsVerts = {
+             // Legs (Using Line Strip)
+             -0.4f, 1.5f, 0.2f,
+             0.0f, 0.0f, 0.0f,//RGB
+             -0.4f, 1.0f, 0.2f,
+             0.0f, 0.0f, 0.0f,//RGB
+              0.4f, 1.0f, 0.2f,
+             0.0f, 0.0f, 0.0f,//RGB
+             0.4f, 1.5f, 0.2f,
+             0.0f, 0.0f, 0.0f//RGB
+            };
 
-         // Eyes
-         -0.5f,-0.7f,0.2f,
-         0.0f,0.0f,0.0f,//RGB
-         0.5f,-0.7f,0.2f,
-         0.0f,0.0f,0.0f,//RGB
 
-        // Mouth Quad - 2 Triangles
-        -0.2f, 0.2f, 0.2f,   
-        0.0f, 0.0f, 0.0f,//RGB  
-        0.2f, 0.2f, 0.2f,   
-        0.0f, 0.0f, 0.0f,//RGB  
-        0.2f, 0.4f, 0.2f,    
-        0.0f, 0.0f, 0.0f,//RGB  
-        -0.2f, 0.2f, 0.2f,
-        0.0f, 0.0f, 0.0f,//RGB  
-        0.2f, 0.4f, 0.2f,  
-        0.0f, 0.0f, 0.0f,//RGB 
-        -0.2f, 0.4f, 0.2f,
-        0.0f, 0.0f, 0.0f,//RGB
-
-        //Right Arm
-        -0.5f,0.0f,0.2f,
-        0.0f,0.0f,0.0f,//RGB
-        -1.4f,-1.0f,0.2f,
-        0.0f,0.0f,0.0f,//RGB
-
-        //Left Arm
-        0.5f,0.0f,0.2f,
-        0.0f,0.0f,0.0f,//RGB
-        1.4f,-1.0f,0.2f,
-        0.0f,0.0f,0.0f,//RGB
-
-        // Legs (Using Line Strip)
-        -0.4f, 1.5f, 0.2f,   
-        0.0f, 0.0f, 0.0f,//RGB
-        -0.4f, 1.0f, 0.2f,   
-        0.0f, 0.0f, 0.0f,//RGB
-         0.4f, 1.0f, 0.2f,   
-        0.0f, 0.0f, 0.0f,//RGB
-        0.4f, 1.5f, 0.2f,   
-        0.0f, 0.0f, 0.0f//RGB
+            cheeseBufferID =  GPU.GenerateBuffer(cheeseVerts); 
+            armsBufferID =  GPU.GenerateBuffer(armsVerts); 
+            legsBufferID =  GPU.GenerateBuffer(legsVerts); 
+            mouthBufferID =  GPU.GenerateBuffer(mouthVerts); 
+            eyesBufferID = GPU.GenerateBuffer(eyesVerts);
 
 
-   };           
-            vertexBufferID = GPU.GenerateBuffer(verts);
+            // View matrix 
+            ViewMatrix = glm.lookAt(
+            new vec3(0, 0, 2),// eye
+            new vec3(0, 0, 0), // center
+            new vec3(0, 1, 0)); // up
+
+            // Model Matrix Initialization
+            ModelMatrix = new mat4(1);
 
             //ProjectionMatrix = glm.perspective(FOV, Width / Height, Near, Far);
-            p = glm.perspective(5.0f,1/1.0f,1.0f, 10.0f);
-            // View matrix 
-            v = glm.lookAt(
-                new vec3(0,0,2),// eye
-                new vec3(0,0,0), // center
-                new vec3(0,1,0)); // up
-            // Model matrix: apply transformations to the model
-            m = new mat4(1);
-            // Our MVP matrix which is a multiplication of our 3 matrices 
-            List<mat4> mvpList = new List<mat4>();
-            mvpList.Add(m);
-            mvpList.Add(v);
-            mvpList.Add(p);
-            mvp = MathHelper.MultiplyMatrices(mvpList);
+            ProjectionMatrix = glm.perspective(-45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
 
+            // Our MVP matrix which is a multiplication of our 3 matrices 
             sh.UseShader();
 
 
             //Get a handle for our "MVP" uniform (the holder we created in the vertex shader)
-            MVP_ID = Gl.glGetUniformLocation(sh.ID, "MVP");
-            //pass the value of the MVP you just filled to the vertex shader
-            Gl.glUniformMatrix4fv(MVP_ID, 1, Gl.GL_FALSE, mvp.to_array());
+            ShaderModelMatrixID = Gl.glGetUniformLocation(sh.ID, "modelMatrix");
+            ShaderViewMatrixID = Gl.glGetUniformLocation(sh.ID, "viewMatrix");
+            ShaderProjectionMatrixID = Gl.glGetUniformLocation(sh.ID, "projectionMatrix");
+
+            Gl.glUniformMatrix4fv(ShaderViewMatrixID, 1, Gl.GL_FALSE, ViewMatrix.to_array());
+            Gl.glUniformMatrix4fv(ShaderProjectionMatrixID, 1, Gl.GL_FALSE, ProjectionMatrix.to_array());
+
+            timer.Start();
         }
 
         public void Draw()
         {
+            sh.UseShader();
             Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
-            
+           
+
+            #region Cheese
+            Gl.glBindBuffer(Gl.GL_ARRAY_BUFFER, cheeseBufferID);
+
+            Gl.glUniformMatrix4fv(ShaderModelMatrixID, 1, Gl.GL_FALSE, ModelMatrix.to_array());
+
             Gl.glEnableVertexAttribArray(0);
-            Gl.glVertexAttribPointer(0, 3, Gl.GL_FLOAT, Gl.GL_FALSE, 6*sizeof(float), (IntPtr)0);
+            Gl.glVertexAttribPointer(0, 3, Gl.GL_FLOAT, Gl.GL_FALSE, 6 * sizeof(float), (IntPtr)0);
             Gl.glEnableVertexAttribArray(1);
-            Gl.glVertexAttribPointer(1, 3, Gl.GL_FLOAT, Gl.GL_FALSE, 6 * sizeof(float), (IntPtr)(3*sizeof(float)));
-             // 5 Primitives Triangles,Points,Lines,Line Strip,Square(using 2 triangles)
-            //pizza slice
-            Gl.glDrawArrays(Gl.GL_TRIANGLES, 0, 6);
+            Gl.glVertexAttribPointer(1, 3, Gl.GL_FLOAT, Gl.GL_FALSE, 6 * sizeof(float), (IntPtr)(3 * sizeof(float)));
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 0, 3);
 
-            //eyes
-            Gl.glPointSize(10);
-            Gl.glDrawArrays(Gl.GL_POINTS, 6, 2);
-
-            //Mouth Quad
-            Gl.glDrawArrays(Gl.GL_TRIANGLES, 8, 6);
-
-            //Arms
-            Gl.glLineWidth(5);
-            Gl.glDrawArrays(Gl.GL_LINES, 14, 4);
-
-            // Draw Legs using Line Strip
-            Gl.glLineWidth(5);
-            Gl.glDrawArrays(Gl.GL_LINE_STRIP, 18, 4);  
 
             Gl.glDisableVertexAttribArray(0);
             Gl.glDisableVertexAttribArray(1);
+            #endregion
+           
+            #region Eyes
+            Gl.glBindBuffer(Gl.GL_ARRAY_BUFFER, eyesBufferID);
+
+            Gl.glUniformMatrix4fv(ShaderModelMatrixID, 1, Gl.GL_FALSE, ModelMatrix.to_array());
+
+            Gl.glEnableVertexAttribArray(0);
+            Gl.glVertexAttribPointer(0, 3, Gl.GL_FLOAT, Gl.GL_FALSE, 6 * sizeof(float), (IntPtr)0);
+            Gl.glEnableVertexAttribArray(1);
+            Gl.glVertexAttribPointer(1, 3, Gl.GL_FLOAT, Gl.GL_FALSE, 6 * sizeof(float), (IntPtr)(3 * sizeof(float)));
+            Gl.glPointSize(10);
+            Gl.glDrawArrays(Gl.GL_POINTS, 0, 2);
 
 
+            Gl.glDisableVertexAttribArray(0);
+            Gl.glDisableVertexAttribArray(1);
+            #endregion
+            #region Arms
+            Gl.glBindBuffer(Gl.GL_ARRAY_BUFFER, armsBufferID);
+
+            Gl.glUniformMatrix4fv(ShaderModelMatrixID, 1, Gl.GL_FALSE, ModelMatrix.to_array());
+
+            Gl.glEnableVertexAttribArray(0);
+            Gl.glVertexAttribPointer(0, 3, Gl.GL_FLOAT, Gl.GL_FALSE, 6 * sizeof(float), (IntPtr)0);
+            Gl.glEnableVertexAttribArray(1);
+            Gl.glVertexAttribPointer(1, 3, Gl.GL_FLOAT, Gl.GL_FALSE, 6 * sizeof(float), (IntPtr)(3 * sizeof(float)));
+
+            Gl.glLineWidth(5);
+            Gl.glDrawArrays(Gl.GL_LINES, 0, 4);
+
+
+            Gl.glDisableVertexAttribArray(0);
+            Gl.glDisableVertexAttribArray(1);
+            #endregion
+            #region Legs
+            Gl.glBindBuffer(Gl.GL_ARRAY_BUFFER, legsBufferID);
+
+            Gl.glUniformMatrix4fv(ShaderModelMatrixID, 1, Gl.GL_FALSE, ModelMatrix.to_array());
+
+            Gl.glEnableVertexAttribArray(0);
+            Gl.glVertexAttribPointer(0, 3, Gl.GL_FLOAT, Gl.GL_FALSE, 6 * sizeof(float), (IntPtr)0);
+            Gl.glEnableVertexAttribArray(1);
+            Gl.glVertexAttribPointer(1, 3, Gl.GL_FLOAT, Gl.GL_FALSE, 6 * sizeof(float), (IntPtr)(3 * sizeof(float)));
+            Gl.glLineWidth(5);
+            Gl.glDrawArrays(Gl.GL_LINE_STRIP, 0, 4);
+
+
+
+            Gl.glDisableVertexAttribArray(0);
+            Gl.glDisableVertexAttribArray(1);
+            #endregion
+            #region Mouth
+            Gl.glBindBuffer(Gl.GL_ARRAY_BUFFER, mouthBufferID);
+
+            Gl.glUniformMatrix4fv(ShaderModelMatrixID, 1, Gl.GL_FALSE, ModelMatrix.to_array());
+
+            Gl.glEnableVertexAttribArray(0);
+            Gl.glVertexAttribPointer(0, 3, Gl.GL_FLOAT, Gl.GL_FALSE, 6 * sizeof(float), (IntPtr)0);
+            Gl.glEnableVertexAttribArray(1);
+            Gl.glVertexAttribPointer(1, 3, Gl.GL_FLOAT, Gl.GL_FALSE, 6 * sizeof(float), (IntPtr)(3 * sizeof(float)));
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 0, 6);
+
+
+
+            Gl.glDisableVertexAttribArray(0);
+            Gl.glDisableVertexAttribArray(1);
+            #endregion
+         
         }
+
         public void Update()
         {
+            timer.Stop();
+            var deltaTime = timer.ElapsedMilliseconds / 1000.0f;
+            rotationAngle += deltaTime * rotationSpeed;
+
+            List<mat4> transformations = new List<mat4>();
+            transformations.Add(glm.translate(new mat4(1), -1 * triangleCenter));
+            transformations.Add(glm.rotate(rotationAngle, new vec3(1, 0, 0)));
+            transformations.Add(glm.translate(new mat4(1), triangleCenter));
+            transformations.Add(glm.translate(new mat4(1), new vec3(translationX, translationY, translationZ)));
+
+            ModelMatrix = MathHelper.MultiplyMatrices(transformations);
+
+            timer.Reset();
+            timer.Start();
         }
+
         public void CleanUp()
         {
             sh.DestroyShader();
--- Graphics/GraphicsForm.Designer.cs
+++ Graphics/GraphicsForm.Designer.cs
@@ -28,7 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.simpleOpenGlControl1 = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.programTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // simpleOpenGlControl1
@@ -41,19 +43,28 @@
             this.simpleOpenGlControl1.BackColor = System.Drawing.Color.Black;
             this.simpleOpenGlControl1.ColorBits = ((byte)(32));
             this.simpleOpenGlControl1.DepthBits = ((byte)(16));
+            this.simpleOpenGlControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.simpleOpenGlControl1.Location = new System.Drawing.Point(0, 0);
+            this.simpleOpenGlControl1.Margin = new System.Windows.Forms.Padding(2);
             this.simpleOpenGlControl1.Name = "simpleOpenGlControl1";
-            this.simpleOpenGlControl1.Size = new System.Drawing.Size(765, 439);
+            this.simpleOpenGlControl1.Size = new System.Drawing.Size(575, 357);
             this.simpleOpenGlControl1.StencilBits = ((byte)(0));
             this.simpleOpenGlControl1.TabIndex = 0;
             this.simpleOpenGlControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.simpleOpenGlControl1_Paint);
+            this.simpleOpenGlControl1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.simpleOpenGlControl1_KeyPress);
+            // 
+            // programTimer
+            // 
+            this.programTimer.Interval = 5;
+            this.programTimer.Tick += new System.EventHandler(this.programTimer_Tick);
             // 
             // GraphicsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(767, 439);
+            this.ClientSize = new System.Drawing.Size(575, 357);
             this.Controls.Add(this.simpleOpenGlControl1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "GraphicsForm";
             this.Text = "Graphics";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GraphicsForm_FormClosing);
@@ -64,6 +75,7 @@
         #endregion
 
         private Tao.Platform.Windows.SimpleOpenGlControl simpleOpenGlControl1;
+        private System.Windows.Forms.Timer programTimer;
     }
 }
 
